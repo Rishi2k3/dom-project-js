c8bfb1cb5dab5f703735326d7387f6e73a9e7bb5
Rishi2k3 <118542258+Rishi2k3@users.noreply.github.com>
2024-09-01 22:12:55 +0530

    project2 edited

--- project2-bmi-calculator/app.js
+++ project2-bmi-calculator/app.js
@@ -2,16 +2,19 @@ function calculatefnc() {
     let height = parseInt(document.getElementById('height').value);
     let weight = parseInt(document.getElementById('weight').value);
     let result = document.getElementById('results');
+    let guide = document.querySelector('.guide');
 
     // Validation for height
     if (isNaN(height) || height <= 0) {
-        result.innerHTML = `Please provide a valid height in cm--> ${height}`;
+        result.innerHTML = `${height}`;
+        guide.innerHTML = " Please provide a valid height in cm";
         return;
     }
 
     // Validation for weight
     if (isNaN(weight) || weight <= 0) {
-        result.innerHTML = `Please provide a valid weight in kg--> ${weight}`;
+        result.innerHTML = `${weight}`;
+        guide.innerHTML = "Please provide a valid weight in kg";
         return;
     }
 
@@ -19,5 +22,14 @@ function calculatefnc() {
     let bmi = (weight / ((height / 100) * (height / 100))).toFixed(2);
 
     // Display the result
+    if (bmi < 18.6) {
+        guide.innerHTML = "Oops you are under Weight";
+    }
+    if (bmi > 24.9) {
+        guide.innerHTML = "Oops you are Over Weight";
+    }
+    if (bmi>=18.6 && bmi<=24.9) {
+        guide.innerHTML = "Congrats!! you are Normal Weighted";
+    }
     result.innerHTML = `Your BMI is ${bmi}.`;
 }
